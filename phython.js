056f7795aae2af914699f7cef35358660e112481
Totoro5 <61136720+Totoro5@users.noreply.github.com>
2020-11-14 22:49:43 +0800

    Create phython.js

---
+++ phython.js
@@ -0,0 +1,7 @@
+＃宠汪汪邀请助力与赛跑助力
+15 10 * * * https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_joy_run.js，tag =宠汪汪邀请助力与赛跑助力，img-url = https：//raw.githubusercontent.com/58xinian /icon/master/jdcww.png，启用= true
+[rewrite_local]
+＃宠汪汪助力更新令牌
+^ https：\ / \ / draw \ .jdfcloud \ .com \ / \ / api \ / user \ / addUser \？code = url脚本-响应正文https://raw.githubusercontent.com/lxk0301/jd_scripts/master /jd_joy_run.js
+＃宠汪汪助力获取令牌
+^ https：\ / \ / draw \ .jdfcloud \ .com \ / \ / api \ / user \ / user \ / detail \？openId = url script-request-header https://raw.githubusercontent.com/lxk0301/ jd_scripts / master / jd_joy_run.js
